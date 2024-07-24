3700624b9b3c4a372e2e615150a3cfaf3f80c469
WilsonCruzz <wilson860731@gmail.com>
2024-07-24 14:37:00 -0400

    create express

---
+++ node/nodeApp/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
